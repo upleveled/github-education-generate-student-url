935efff1ae6739a9707edbad09c72fa8ccc72b44
Karl Horky <karl.horky@gmail.com>
2020-04-14 18:27:03 +0200

    Add shebang and comment about printf / echo -n

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [ ! -f .env ]; then
   echo "\033[0;31m.env file not found!\033[0m\nPlease copy .env.example to .env and fill in the values."
   exit 1
@@ -11,5 +13,8 @@ if [ -z "$student_id" ]; then
 fi
 
 eval $(egrep -v '^#' .env)
+
+# Use "printf" instead of "echo -n" for compatibility / portability
+# https://apple.stackexchange.com/a/173873/36202
 signature=$(printf ${school_id}${student_id} | openssl dgst -sha256 -hex -hmac ${secret_key})
 echo "https://education.github.com/student/verify?school_id=${school_id}&student_id=${student_id}&signature=${signature}"
